5e787e23b46b867b3d998ff1d40a098c79aeb52e
tsoingkammy <3486117125@qq.com>
2018-01-25 11:54:17 +0800

    第一次上传

--- myblog/settings.py
+++ myblog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7zi@$4hcg#s24ewiwa9bv_&mdv_w!6rg9z&taqd9!5iizsnqlm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.tsoingkam.cn']
 
 
 # Application definition
@@ -130,3 +130,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
